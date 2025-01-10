9c62f7f72eeb7aa3a07da7d2b5051a2007d2616a
TierTheTora <160141870+TierTheTora@users.noreply.github.com>
2025-01-10 23:30:59 +0100

    Add files via upload

--- output.asm
+++ output.asm
@@ -2,12 +2,12 @@ section .text
 global _start
 _start:
 section .data
-str1: db "Hello, World!", 0
+str1: db "Enter your name: ", 0
 section .text
 mov rax, 1
 mov rdi, 1
 mov rsi, str1
-mov rdx, 16
+mov rdx, 20
 syscall
 section .bss
 my_inp: resb 100
@@ -16,6 +16,22 @@ mov rax, 0
 mov rdi, 0
 mov rsi, my_inp
 mov rdx, 255
+syscall
+section .data
+str3: db " ", 0
+section .text
+mov rax, 1
+mov rdi, 1
+mov rsi, str3
+mov rdx, 4
+syscall
+section .data
+str5: db "Hi, ", 0
+section .text
+mov rax, 1
+mov rdi, 1
+mov rsi, str5
+mov rdx, 7
 syscall
 section .text
 mov rax, 1
